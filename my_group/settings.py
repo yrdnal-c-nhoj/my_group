bfe273cea9598cd0ab47525adc9b92429aa11cf8
John-Landry <johnlandry33@gmail.com>
2024-05-11 17:49:14 -0400

    x

--- my_group/settings.py
+++ my_group/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members',
+ #   'django_registration',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window
